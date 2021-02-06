a1459e6519a36e4554ce8dcae19150ff447af87d
jarryd smyth <jarryd@myconnect.com.au>
2021-02-06 13:08:26 +1100

    Clean up old code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,15 +5,7 @@ class PagesController < ApplicationController
   def dashboard
     @my_bookings = current_user.bookings
     
-    @my_inspection_bookings = []
     @my_inspections = current_user.inspections
-
-
-    
-    # @my_inspections.each do |inspection| 
-    #   @my_inspection_bookings << inspection.bookings
-    # end
-
   end
 
 end
