b38d41a31d25d64bbce92a869266100fa36288b8
jarryd smyth <jarryd@myconnect.com.au>
2021-02-06 12:40:55 +1100

    Dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,17 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def dashboard
+    @my_bookings = current_user.bookings
+    
+    @my_inspection_bookings = []
+    @my_inspections = current_user.inspections
+
+    @my_inspections.each do |inspection| 
+      @my_inspection_bookings << inspection.bookings
+    end
+
+  end
+
 end
--- app/models/inspection.rb
+++ app/models/inspection.rb
@@ -1,4 +1,5 @@
 class Inspection < ApplicationRecord
   # validates :description, location presence: true
   belongs_to :user
+  has_many :bookings, dependent: :destroy
 end
