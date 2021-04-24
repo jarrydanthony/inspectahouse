b3c49835501b0a8fd158d650542d9aa996080485
Alex M <alexmaksimets@gmail.com>
2021-04-24 22:50:51 +1000

    added time picker

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -1,6 +1,12 @@
 import flatpickr from "flatpickr";
 
 const initFlatpickr = () => {
-  flatpickr(".datepicker", {});
+  flatpickr(".datepicker.js-flatpickr-dateTime", {
+    enableTime:true,
+    dateTime: true,
+    altInput: true,
+    altFormat: 'd/m/Y H:i',
+    dateFormat: 'Y-m-d H:i',
+  });
 }
 export { initFlatpickr };
